e29af803e69545e2fc8896602006c4dda8d91b66
Nicole Cox <nicoleanalisecox@gmail.com>
2021-07-06 21:59:05 +0100

    add design files, update slider event, basic style

---
+++ script.js
@@ -0,0 +1,42 @@
+const data = [
+  {
+    "pageviews": "10K",
+    "price": 8,
+  },
+  {
+    "pageviews": "50K",
+    "price": 12,
+  },
+    {
+    "pageviews": "100K",
+    "price": 16,
+  },
+    {
+    "pageviews": "500K",
+    "price": 24,
+  },
+  {
+    "pageviews": "10M",
+    "price": 36,
+  },
+    {
+    "pageviews": "test",
+    "price": "test",
+  }
+]
+
+const price = document.querySelector('#price');
+const pageviews = document.querySelector('#pageviews');
+const slider = document.querySelector('#priceRange');
+
+slider.addEventListener('input', (e) => {
+
+    pageviews.innerText = `${data[e.target.value].pageviews} PAGEVIEWS`;
+
+    price.innerText = `$ ${data[e.target.value].price}.00`;
+
+})
+
+
+
+
